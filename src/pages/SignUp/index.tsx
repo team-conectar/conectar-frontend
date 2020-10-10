4e1e919a0f79c5b825ae91877155ebeee838686c
Lucas Emanuel <lucasemanuelss@hotmail.com>
2020-10-10 14:28:53 -0300

    Fix: Separating forms on signup
    - Now user will be signed up when finishing the first step of form

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,29 +1,32 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { BodySignUp } from './styles';
-import logo from '../../assets/image/logo_fundoClaro.svg';
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import { Link } from 'react-router-dom';
-import Button from '../../components/Button';
-import ToggleSwitch from '../../components/ToggleSwitch';
-import { ReactFacebookLoginInfo, ReactFacebookFailureResponse } from 'react-facebook-login';
-import FacebookLogin from 'react-facebook-login';
-import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
-import { FcGoogle } from 'react-icons/fc';
-import { FaFacebookF } from 'react-icons/fa';
-import { useHistory } from 'react-router';
-import { monthOptions, yearOptions } from '../../utils/dates';
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import { BodySignUp } from "./styles";
+import logo from "../../assets/image/logo_fundoClaro.svg";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import ToggleSwitch from "../../components/ToggleSwitch";
+import {
+  ReactFacebookLoginInfo,
+  ReactFacebookFailureResponse,
+} from "react-facebook-login";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin, {
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from "react-google-login";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookF } from "react-icons/fa";
+import { useHistory } from "react-router";
+import { monthOptions, yearOptions } from "../../utils/dates";
 import axios, { AxiosError } from "axios";
-
 
 interface renderFacebook {
   onClick: () => void;
   disabled?: boolean;
 }
 
-
 function SignUp() {
-
   const history = useHistory();
   const [formData, setFormData] = useState({
     email: "",
@@ -31,7 +34,6 @@ function SignUp() {
     nome: "",
     username: "",
     password: "",
-    celular: "",
     year: "",
     month: "",
     day: "",
@@ -43,10 +45,10 @@ function SignUp() {
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
     const target = event.target;
     const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
 
-    setFormData({ ...formData, [name]: value })
-  };
+    setFormData({ ...formData, [name]: value });
+  }
 
   function handleSelectChange(event: ChangeEvent<HTMLSelectElement>) {
     /**
@@ -62,59 +64,105 @@ function SignUp() {
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
 
-    const { email, telefone, nome, username, year, password, month, day, aliado, celular, colaborador, idealizador } = formData;
+    const {
+      email,
+      telefone,
+      nome,
+      username,
+      year,
+      password,
+      month,
+      day,
+    } = formData;
 
     const data_nascimento = `${year}-${month}-${day}`;
 
     const data = new FormData();
 
-
-    data.append('data_nascimento', data_nascimento);
-    data.append('email', email);
-    data.append('telefone', telefone);
-    data.append('nome', nome);
-    data.append('username', username);
-    data.append('password', password);
-    data.append('celular', celular);
+    data.append("data_nascimento", data_nascimento);
+    data.append("email", email);
+    data.append("telefone", telefone);
+    data.append("nome", nome);
+    data.append("username", username);
+    data.append("password", password);
     try {
-      const res = await axios
-        .post("/api/signup", data);
+      const res = await axios.post("/api/signup", data);
+    } catch (error) {
+      return error.response.data.detail;
+    }
+  }
+
+  async function handleSecondSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { year, month, day } = formData;
+
+    const data_nascimento = `${year}-${month}-${day}`;
+
+    const data = { ...formData, data_nascimento };
+    try {
+      const res = await axios.put("/api/v1/pessoas", data, {
+        withCredentials: true,
+      });
       history.push("/experienceareas");
     } catch (error) {
       return error.response.data.detail;
     }
-
   }
 
   const [showNextStep, setShowNextStep] = useState<boolean>(false);
 
-  const responseFacebook = (resposta: ReactFacebookLoginInfo | ReactFacebookFailureResponse) => {
+  const responseFacebook = (
+    resposta: ReactFacebookLoginInfo | ReactFacebookFailureResponse
+  ) => {
     console.log(resposta);
-  }
-  const responseGoogle = (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+  };
+  const responseGoogle = (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
     console.log(response);
-  }
+  };
 
   return (
     <BodySignUp showSecondStep={showNextStep}>
       <form onSubmit={handleSubmit} className="area-central container">
-        <Link to="/"><img src={logo} alt="logo" /></Link>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
         <div className="primeira-etapa">
-
           <img src="" alt="#" className="area-img" />
 
-
           <div className="area-form">
-
-
             <h1>Criar sua conta</h1>
-            <Input name="nome" label="Nome Completo" onChange={handleInputChange} />
-            <Input type="email" name="email" label="E-mail" onChange={handleInputChange} />
+            <Input
+              name="nome"
+              label="Nome Completo"
+              onChange={handleInputChange}
+            />
+            <Input
+              type="email"
+              name="email"
+              label="E-mail"
+              onChange={handleInputChange}
+            />
             <section>
-              <Input name="username" label="Nome de usuário" onChange={handleInputChange} />
-              <Input type="password" name="password" label="Senha" onChange={handleInputChange} />
+              <Input
+                name="username"
+                label="Nome de usuário"
+                onChange={handleInputChange}
+              />
+              <Input
+                type="password"
+                name="password"
+                label="Senha"
+                onChange={handleInputChange}
+              />
             </section>
-            <p>Ao prosseguir, você concorda com os <Link to="#">Termos de Uso</Link> e <Link to="#">Política de Privacidade.</Link></p>
+            <p>
+              Ao prosseguir, você concorda com os{" "}
+              <Link to="#">Termos de Uso</Link> e{" "}
+              <Link to="#">Política de Privacidade.</Link>
+            </p>
 
             <section>
               <Link to="login">Já tem uma conta?</Link>
@@ -122,12 +170,17 @@ function SignUp() {
                 onClick={() => setShowNextStep(true)}
                 theme="primary-yellow"
                 type="button"
-                disabled={formData.nome === "" || formData.email === "" || formData.username === "" || formData.password === ""}
-              >Continuar
+                disabled={
+                  formData.nome === "" ||
+                  formData.email === "" ||
+                  formData.username === "" ||
+                  formData.password === ""
+                }
+              >
+                Continuar
               </Button>
             </section>
             <section>
-
               <FacebookLogin
                 appId="1088597931155576"
                 autoLoad={true}
@@ -136,106 +189,130 @@ function SignUp() {
                 cssClass="facebook-button"
                 textButton="Cadastre-se com Facebook"
                 icon={<FaFacebookF />}
-
               />
               <GoogleLogin
                 clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                render={renderProps => (
+                render={(renderProps) => (
                   <button
                     className="google-button"
                     onClick={renderProps.onClick}
                     disabled={renderProps.disabled}
                   >
                     <FcGoogle />
-                  Inscreva-se com Google
+                    Inscreva-se com Google
                   </button>
                 )}
                 buttonText="Login"
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
+                cookiePolicy={"single_host_origin"}
               />
-
-
             </section>
           </div>
         </div>
-        <div className="segunda-etapa">
-          <legend>
-            <h1>Bem vindo(a) ao Conectar</h1>
-          </legend>
-          <section>
-            <Input type="tel" name="telefone" label="Celular" onChange={handleInputChange}></Input>
-            <Select
-              label="Data de Nascimento"
-              name="year"
-              defaultOption="Ano"
-              options={yearOptions}
-              onChange={handleSelectChange}
-            />
-            <Select
-              name="month"
-              defaultOption="Mês"
-              options={monthOptions}
-              onChange={handleSelectChange}
-            />
-            <Input 
-              type="number" 
-              name="day" 
-              placeholder="Dia" 
-              onChange={handleInputChange}
-            />
-
-          </section>
-          <section>
-            <legend>Tipo de Perfil</legend>
-            <span>Selecione um ou mais tipos</span>
-          </section>
-          <section className="tipo-perfil">
-            <fieldset>
-
-              <legend>Idealizador</legend>
-              <aside>
-                <p>xxxxxxxxxxxxxxxx xxx xxxx</p>
-                <ToggleSwitch name="idealizador" id="idealizador" onChange={handleInputChange} />
-
-              </aside>
-            </fieldset>
-            <fieldset>
-
-              <legend>Colaborador</legend>
-              <aside>
-                <p>xxxxxxxxxxxxxxxx xxx xxxx</p>
-                <ToggleSwitch name="colaborador" id="colaborador" onChange={handleInputChange} />
-
-
-              </aside>
-            </fieldset>
-            <fieldset>
-              <legend>Aliado</legend>
-              <aside>
-                <p>xxxxxxxxxxxxxxxx xxx xxxx</p>
-                <ToggleSwitch name="aliado" id="aliado" onChange={handleInputChange} />
-
-              </aside>
-            </fieldset>
-          </section>
-          <section>
-            <button
-              className="voltar"
-              type="button"
-              onClick={() => setShowNextStep(false)}
-            >Voltar</button>
-            <Button
-              theme="primary-yellow"
-              type="submit"
-              disabled={formData.telefone === "" || formData.day === "" || formData.month === "" || formData.year === "" || !(formData.aliado || formData.colaborador || formData.idealizador)}
-              onClick={() => history.push("/experienceareas")}
-            >Continuar</Button>
-          </section>
-        </div>
       </form>
+      {showNextStep && (
+        <form onSubmit={handleSecondSubmit} className="area-central container">
+          <div className="segunda-etapa">
+            <legend>
+              <h1>Bem vindo(a) ao Conectar</h1>
+            </legend>
+            <section>
+              <Input
+                type="tel"
+                name="telefone"
+                label="Celular"
+                onChange={handleInputChange}
+              ></Input>
+              <Select
+                label="Data de Nascimento"
+                name="year"
+                defaultOption="Ano"
+                options={yearOptions}
+                onChange={handleSelectChange}
+              />
+              <Select
+                name="month"
+                defaultOption="Mês"
+                options={monthOptions}
+                onChange={handleSelectChange}
+              />
+              <Input
+                type="number"
+                name="day"
+                placeholder="Dia"
+                onChange={handleInputChange}
+              />
+            </section>
+            <section>
+              <legend>Tipo de Perfil</legend>
+              <span>Selecione um ou mais tipos</span>
+            </section>
+            <section className="tipo-perfil">
+              <fieldset>
+                <legend>Idealizador</legend>
+                <aside>
+                  <p>xxxxxxxxxxxxxxxx xxx xxxx</p>
+                  <ToggleSwitch
+                    name="idealizador"
+                    id="idealizador"
+                    onChange={handleInputChange}
+                  />
+                </aside>
+              </fieldset>
+              <fieldset>
+                <legend>Colaborador</legend>
+                <aside>
+                  <p>xxxxxxxxxxxxxxxx xxx xxxx</p>
+                  <ToggleSwitch
+                    name="colaborador"
+                    id="colaborador"
+                    onChange={handleInputChange}
+                  />
+                </aside>
+              </fieldset>
+              <fieldset>
+                <legend>Aliado</legend>
+                <aside>
+                  <p>xxxxxxxxxxxxxxxx xxx xxxx</p>
+                  <ToggleSwitch
+                    name="aliado"
+                    id="aliado"
+                    onChange={handleInputChange}
+                  />
+                </aside>
+              </fieldset>
+            </section>
+            <section>
+              <button
+                className="voltar"
+                type="button"
+                onClick={() => setShowNextStep(false)}
+              >
+                Voltar
+              </button>
+              <Button
+                theme="primary-yellow"
+                type="submit"
+                disabled={
+                  formData.telefone === "" ||
+                  formData.day === "" ||
+                  formData.month === "" ||
+                  formData.year === "" ||
+                  !(
+                    formData.aliado ||
+                    formData.colaborador ||
+                    formData.idealizador
+                  )
+                }
+              >
+                Continuar
+              </Button>
+            </section>
+          </div>
+        </form>
+      )}
     </BodySignUp>
-  )
+  );
 }
 export default SignUp;
