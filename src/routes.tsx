e81287b38c9ff4678ffe4267663a3b62bd2b50bb
jefer <jefer@pop-os.localdomain>
2021-01-23 16:33:49 -0300

    ✨ feat: Create global styles with styled-components

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
+import React from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import ProfileFeatures from './pages/ProfileFeatures'
-import ExperienceAreas from './pages/ExperienceAreas';
-import MasteryTools from './pages/MasteryTools';
-import CreateProject from './pages/CreateProject';
-import ApproveProject from './pages/ApproveProject';
-import Projects from './pages/Projects';
-import Profiles from './pages/Profiles';
-
-import { AuthProvider } from './context/AuthContext';
-
+import ExperienceAreas from './pages/ExperienceAreas'
+import MasteryTools from './pages/MasteryTools'
+import CreateProject from './pages/CreateProject'
+import ApproveProject from './pages/ApproveProject'
+import Projects from './pages/Projects'
+import Profiles from './pages/Profiles'
+import { GlobalStyle } from './assets/style/global'
+import { AuthProvider } from './context/AuthContext'
+import Explorer from './pages/Explorer'
 function Routes() {
   return (
     <AuthProvider>
@@ -19,15 +19,16 @@ function Routes() {
         <Route exact path="/" component={Home} />
         <Route path="/signup/:step" component={SignUp} />
         <Route path="/profilefeatures" component={ProfileFeatures} />
-        <Route path="/experienceareas" component={ ExperienceAreas} />
-        <Route path="/masterytools" component={ MasteryTools} />
-        <Route path="/createproject" component={ CreateProject} />
-        <Route path="/approveproject" component={ ApproveProject} />
-        <Route path="/projects/:id" component={ Projects} />
-        <Route path="/profiles/:id" component={ Profiles} />
+        <Route path="/experienceareas" component={ExperienceAreas} />
+        <Route path="/masterytools" component={MasteryTools} />
+        <Route path="/createproject" component={CreateProject} />
+        <Route path="/approveproject" component={ApproveProject} />
+        <Route path="/projects/:id" component={Projects} />
+        <Route path="/profiles/:id" component={Profiles} />
+        <Route path="/explorer" component={Explorer} />
+        <GlobalStyle />
       </BrowserRouter>
     </AuthProvider>
   )
-
 }
-export default Routes;
+export default Routes
