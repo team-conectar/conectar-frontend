112c6a520fcda913d8da53b40cb48ceac9ff3a36
jefer <jefer@pop-os.localdomain>
2021-02-17 12:45:45 -0300

    🐛 - fix: Refactoring input validation

--- src/components/ToggleSwitch/index.tsx
+++ src/components/ToggleSwitch/index.tsx
@@ -2,44 +2,59 @@ import React, { InputHTMLAttributes, useEffect, useRef } from 'react'
 import { useField } from '@unform/core'
 import { BodySwitch } from './styles'
 
+interface IOptionsCheckbox {
+  id: string
+  value: string
+  label: string
+}
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string
-  label?: string
+  options: Array<IOptionsCheckbox>
 }
 
-const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
-  const inputRefs = useRef<HTMLInputElement>(null)
+const Input: React.FC<InputProps> = ({ name, options, ...rest }) => {
+  const inputRefs = useRef<HTMLInputElement[]>([])
   const { fieldName, registerField, defaultValue = [] } = useField(name)
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRefs.current,
-      getValue: (ref: HTMLInputElement) => {
-        return ref.checked ? ref.value : ''
+      getValue: (refs: HTMLInputElement[]) => {
+        return refs.filter(ref => ref.checked).map(ref => ref.value)
       },
-      clearValue: (ref: HTMLInputElement) => {
-        ref.checked = false
+      clearValue: (refs: HTMLInputElement[]) => {
+        refs.forEach(ref => {
+          ref.checked = false
+        })
       },
-      setValue: (ref: HTMLInputElement, value: string) => {
-        if (value === ref.value) {
-          ref.checked = true
-        }
+      setValue: (refs: HTMLInputElement[], values: string[]) => {
+        refs.forEach(ref => {
+          if (values.includes(ref.id)) {
+            ref.checked = true
+          }
+        })
       },
     })
   }, [defaultValue, fieldName, registerField])
   return (
     <BodySwitch>
-      <label htmlFor={fieldName}>{label}</label>
-
-      <input
-        ref={inputRefs}
-        type="checkbox"
-        id={fieldName}
-        className="checkbox"
-        defaultValue={defaultValue}
-        {...rest}
-      />
-      <label htmlFor={fieldName} className="switch"></label>
+      {options.map((option, index) => (
+        <label htmlFor={option.id} key={option.id}>
+          {option.label}
+          <input
+            className="checkbox"
+            defaultChecked={defaultValue.find((dv: string) => dv === option.id)}
+            ref={ref => {
+              inputRefs.current[index] = ref as HTMLInputElement
+            }}
+            value={option.value}
+            type="checkbox"
+            id={option.id}
+            {...rest}
+          />
+          <label htmlFor={fieldName} className="switch" />
+        </label>
+      ))}
     </BodySwitch>
   )
 }
--- src/components/ToggleSwitch/styles.ts
+++ src/components/ToggleSwitch/styles.ts
@@ -1,43 +1,48 @@
 import styled from 'styled-components'
 
 export const BodySwitch = styled.div`
-  display: flex;
-  flex-flow: row-reverse wrap;
-  align-items: center;
-  justify-content: center;
-  margin: 0.6rem;
   label {
-    margin-left: 0.8rem;
-  }
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 46px;
-    height: 18px;
-    background-color: white;
-    box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.1);
-
-    border-radius: 20px;
-    cursor: pointer;
-    ::after {
-      content: '';
-      position: absolute;
-      width: 22px;
-      height: 22px;
-      border-radius: 50%;
-      background-color: var(--green);
-      top: -2px;
-      left: -1px;
+    display: flex;
+    flex-flow: row-reverse wrap;
+    align-items: center;
+    justify-content: center;
+    margin: 0.6rem;
+    label {
+      margin-left: 0.8rem;
+    }
+    .switch {
+      position: relative;
+      display: inline-block;
+      width: 46px;
+      height: 18px;
+      background: linear-gradient(to left, white 50%, var(--textGreen) 50%);
+      background-size: 200% 100%;
+      background-position: right bottom;
+      box-shadow: var(--boxShadow);
       transition: all 0.3s;
+      border-radius: 20px;
+      cursor: pointer;
+      ::after {
+        content: '';
+        position: absolute;
+        width: 22px;
+        height: 22px;
+        border-radius: 50%;
+        background-color: var(--green);
+        top: -2px;
+        left: -1px;
+        transition: all 0.3s;
+      }
     }
-  }
-  .checkbox:checked + .switch {
-    background- ::after {
-      left: 25px;
-      right: 0;
+    .checkbox:checked + .switch {
+      background-position: left bottom;
+      ::after {
+        left: 25px;
+        right: 0;
+      }
     }
-  }
-  .checkbox {
-    display: none;
+    .checkbox {
+      display: none;
+    }
   }
 `
