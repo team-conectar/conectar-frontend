3a00f0e7edfab81c5011855cc5304a8234773618
jefer <jefer@pop-os.localdomain>
2021-03-11 22:26:36 -0300

    🔧 - chore: Make it possible to hide the project owner

--- src/components/UI/NavBar/styles.ts
+++ src/components/UI/NavBar/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { DropdownContent } from '../Dropdown/styles'
 
 export const BodyNavBar = styled.nav`
