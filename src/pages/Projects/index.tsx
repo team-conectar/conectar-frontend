6d3130161da03afc12f61c4e043714d987ad6a3e
souzjfe <souzajefison@gmail.com>
2021-05-07 18:56:26 -0300

    🐛 - fix: show component of notification invite any time

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -101,15 +101,28 @@ const Projects: React.FC = () => {
     vacancies[0],
   )
   function handleDeclineInvitation(pessoa_projeto_id: number) {
-    api.put(`api/v1/pessoa_projeto/${pessoa_projeto_id}`, {
-      situacao: 'RECUSADO',
-    })
+    api
+      .put(`api/v1/pessoa_projeto/${pessoa_projeto_id}`, {
+        situacao: 'RECUSADO',
+      })
+      .then(() => {
+        vacancyDetail &&
+          setVacancyDetail({ ...vacancyDetail, situacao: 'RECUSADO' })
+      })
   }
-  function handleAcceptInvitation(pessoa_projeto_id: number) {
-    api.put(`api/v1/pessoa_projeto/${pessoa_projeto_id}`, {
-      situacao: 'ACEITO',
-    })
-  }
+  const handleAcceptInvitation = useCallback(
+    (pessoa_projeto_id: number) => {
+      api
+        .put(`api/v1/pessoa_projeto/${pessoa_projeto_id}`, {
+          situacao: 'ACEITO',
+        })
+        .then(() => {
+          vacancyDetail &&
+            setVacancyDetail({ ...vacancyDetail, situacao: 'ACEITO' })
+        })
+    },
+    [vacancyDetail],
+  )
   const formRef = useRef<FormHandles>(null)
   useEffect(() => {
     const res = [
@@ -449,33 +462,35 @@ const Projects: React.FC = () => {
           )}
         </DivTags>
       </DivSobre>
-      {vacanciesList && vacancyDetail?.pessoa_id === user.id && (
-        <DivConvite>
-          <figure>
-            <img
-              src={urlConvite}
-              alt="Mulher apertando a mão de um homem simbolizando um acordo"
-            />
-            <figcaption>
-              Você tem apenas {3} dias para responder este covnite
-            </figcaption>
-          </figure>
-          <aside>
-            <Button
-              theme="secondary"
-              onClick={() => handleDeclineInvitation(vacancyDetail?.id)}
-            >
-              Recusar
-            </Button>
-            <Button
-              theme="primary"
-              onClick={() => handleAcceptInvitation(vacancyDetail?.id)}
-            >
-              Aceitar
-            </Button>
-          </aside>
-        </DivConvite>
-      )}
+      {vacanciesList &&
+        vacancyDetail?.situacao === 'PENDENTE_COLABORADOR' &&
+        vacancyDetail?.pessoa_id === user.id && (
+          <DivConvite>
+            <figure>
+              <img
+                src={urlConvite}
+                alt="Mulher apertando a mão de um homem simbolizando um acordo"
+              />
+              <figcaption>
+                Você tem apenas {3} dias para responder este covnite
+              </figcaption>
+            </figure>
+            <aside>
+              <Button
+                theme="secondary"
+                onClick={() => handleDeclineInvitation(vacancyDetail?.id)}
+              >
+                Recusar
+              </Button>
+              <Button
+                theme="primary"
+                onClick={() => handleAcceptInvitation(vacancyDetail?.id)}
+              >
+                Aceitar
+              </Button>
+            </aside>
+          </DivConvite>
+        )}
       <DivVagas showVagas={vacanciesList}>
         <section>
           <legend>
