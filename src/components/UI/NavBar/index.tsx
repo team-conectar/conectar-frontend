37dde07333cac723148a3bdcc116a21d6fe1d2d3
souzjfe <souzajefison@gmail.com>
2021-05-13 20:16:08 -0300

    🐛 - fix: Show incorrect notifications sequence

--- src/components/UI/NavBar/index.tsx
+++ src/components/UI/NavBar/index.tsx
@@ -72,7 +72,8 @@ const NotificationsButton = () => {
     const res = api
       .get(`api/v1/notificacao/destinatario?destinatario_id=${user.id}`)
       .then((response: AxiosResponse<INotification[]>) => {
-        const res = response.data.reverse()
+        const res = response.data
+        res.reverse()
         setNotifications(res)
       })
       .catch((err: AxiosError) => {
