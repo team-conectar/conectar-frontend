0a2e0356deb39e8909233fb4120bc69fd7ebd033
souzjfe <souzajefison@gmail.com>
2021-07-06 17:43:26 -0300

    🐛 - fix: Facebook login

--- src/components/UI/Login/index.tsx
+++ src/components/UI/Login/index.tsx
@@ -148,7 +148,7 @@ const Login: React.FC<loginProps> = ({ onSuccessLogin }) => {
           cssClass="facebook-button"
           icon={<FaFacebook />}
         />
-        <GoogleLogin
+        {/* <GoogleLogin
           clientId="1027346829762-a6tjn6i5a8r50nn0cskrg4sholipvt5j.apps.googleusercontent.com"
           render={renderProps => (
             <button
@@ -163,7 +163,7 @@ const Login: React.FC<loginProps> = ({ onSuccessLogin }) => {
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
           cookiePolicy={'single_host_origin'}
-        />
+        /> */}
       </aside>
       <p>
         Novo no Conectar? <Link to="/cadastrar/1">Crie uma conta</Link>
